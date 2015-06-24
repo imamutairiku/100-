95852a209669633b4773c0af9b09fe8e497afc14
imamutairiku <imamu_tairiku@yahoo.co.jp>
2015-06-24 22:09:03 +0900

    1
    11

--- 100回めざまし！/ViewController.swift
+++ 100回めざまし！/ViewController.swift
@@ -1,102 +1,83 @@
 //
 //  ViewController.swift
-//  Utillity001
+//  UIKit008
 //
 
 import UIKit
-import AVFoundation
 
-class ViewController: UIViewController, AVAudioPlayerDelegate {
-    
-    //時間計測用の変数.
-    var cnt : Float = 0
-    
-    //時間表示用のラベル.
-    var myLabel : UILabel!
-    
-    //タイマー.
-    var timer : NSTimer!
-    
-    var audioPlayer : AVAudioPlayer!
+class ViewController: UIViewController {
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         
-        //再生する音源のURLを生成
-        let soundFilePath : NSString =
-        NSBundle.mainBundle().pathForResource("clap", ofType: "wav")!
-        let fileURL : NSURL = NSURL(fileURLWithPath: soundFilePath)!
+        // アラート表示の許可をもらう.
+        UIApplication.sharedApplication().registerUserNotificationSettings(
+            UIUserNotificationSettings(
+                forTypes:UIUserNotificationType.Sound | UIUserNotificationType.Alert,
+                categories: nil)
+        )
+    
         
-        //AVAudioPlayerのインスタント化
-        audioPlayer = AVAudioPlayer(contentsOfURL: fileURL, error: nil)
+        // 時間をおいてNotificationを発火するボタンを作成する.
+        let myNotificationFireButton: UIButton = UIButton(frame: CGRectMake(0,0,200,80))
+        myNotificationFireButton.backgroundColor = UIColor.blueColor()
+        myNotificationFireButton.layer.masksToBounds = true
+        myNotificationFireButton.setTitle("Notification(Fire)", forState: .Normal)
+        myNotificationFireButton.layer.cornerRadius = 20.0
+        myNotificationFireButton.layer.position = CGPoint(x: self.view.bounds.width/2, y:400)
+        myNotificationFireButton.addTarget(self, action: "onClickMyButton:", forControlEvents: .TouchUpInside)
+        myNotificationFireButton.tag = 2
         
-
-        //Timerが◯分になると音が鳴る
-        timer = 0:00:30 (){
-        audioPlayer.play()
+        // ViewにButtonを追加する.
+        view.addSubview(myNotificationFireButton)
+    }
+        /*
+        ボタンイベント
+        */
+        internal func onClickMyButton(sender: UIButton){
+            println("onClickMyButton")
+            if sender.tag == 2 {
+            showNotificationFire()
         }
-        
-
-        
-        //ラベルを作る.
-        myLabel = UILabel(frame: CGRectMake(0,0,200,50))
-        myLabel.backgroundColor = UIColor.orangeColor()
-        myLabel.layer.masksToBounds = true
-        myLabel.layer.cornerRadius = 20.0
-        myLabel.text = "Time:\(cnt)"
-        myLabel.textColor = UIColor.whiteColor()
-        myLabel.shadowColor = UIColor.grayColor()
-        myLabel.textAlignment = NSTextAlignment.Center
-        myLabel.layer.position = CGPoint(x: self.view.bounds.width/2,y: 200)
-        self.view.backgroundColor = UIColor.cyanColor()
-        self.view.addSubview(myLabel)
-        
-        //タイマー停止ボタンを作る.
-        let myButton = UIButton(frame: CGRectMake(0, 0, 200, 50))
-        myButton.layer.masksToBounds = true
-        myButton.layer.cornerRadius = 20.0
-        myButton.backgroundColor = UIColor.blueColor()
-        myButton.setTitle("Stop Timer", forState: UIControlState.Normal)
-        myButton.layer.position = CGPointMake(self.view.center.x, self.view.center.y + 100)
-        myButton.addTarget(self, action: "onMyButtonClick:", forControlEvents: UIControlEvents.TouchUpInside)
-        self.view.addSubview(myButton)
-
-        //タイマーを作る.
-        timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "onUpdate:", userInfo: nil, repeats: true)
     }
     
-    //ボタンが押された時に呼ばれるメソッド.
-    func onMyButtonClick(sender : UIButton){
-        //timerが動いてるなら.
-        if timer.valid == true {
-        
-        //timerを破棄する.
-            timer.invalidate()
+        /*
+        Show Notification(10 sec後に発火)
+        */
+        private func showNotificationFire(){
+             println("showNotificationFire")
             
-        //ボタンのタイトル変更.
-            sender.setTitle("Start Timer", forState: UIControlState.Normal)
+            // Notificationの生成する.
+            let myNotification: UILocalNotification = UILocalNotification()
+            
+            // メッセージを代入する.
+            myNotification.alertBody = "TEST(Fire)"
+            
+            // 再生サウンドを設定する.
+            myNotification.soundName = UILocalNotificationDefaultSoundName
+            
+            // Timezoneを設定する.
+            myNotification.timeZone = NSTimeZone.defaultTimeZone()
+            
+            // 10秒後に設定する.
+            myNotification.fireDate = NSDate(timeIntervalSinceNow: 10)
+            
+            // Notificationを表示する.
+            UIApplication.sharedApplication().scheduleLocalNotification(myNotification)
         }
-            
-        else{
-            
-            //timerを生成する.
-            timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "onUpdate:", userInfo: nil, repeats: true)
-            
-            //ボタンのタイトル変更.
-            sender.setTitle("Stop Timer", forState: UIControlState.Normal)
-        }
-
-    }
     
-    //NSTimerIntervalで指定された秒数毎に呼び出されるメソッド.
-    func onUpdate(timer : NSTimer){
-        
-        cnt += 0.1
-        
-        //桁数を指定して文字列を作る.
-        let str = "Time:".stringByAppendingFormat("%.1f",cnt)
-        
-        myLabel.text = str
-        
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
     
 }
+     func changeDatePicker(sender: UIDatePicker) {
+        let df = NSDateFormatter()
+        df.dateFormat = "HH:mm:ss"
+        var dateStr = df.stringFromDate(sender.date)
+        println(dateStr)
+    
+    }
+
+
